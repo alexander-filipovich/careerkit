91e5e9ca953b1fefa8f5304fae595f656c3034b4
alexander-filipovich <alexander.filipovich.work@gmail.com>
2026-03-14 16:12:37 +0100

    setup updated

--- setup_venv.sh
+++ setup_venv.sh
@@ -5,29 +5,27 @@ deactivate
 VENV_DIR="venv"
 REQ_FILE="requirements.txt"
 
+deactivate
+echo "venv is ready and dependencies are installed!"
 if [ ! -d "$VENV_DIR" ]; then
     echo "venv not found. Creating new environment..."
     python3 -m venv "$VENV_DIR"
-else
-    echo "venv found. Updating dependencies..."
-    source "$VENV_DIR/bin/activate"
-    pip install --upgrade pip
-    pip install -r "$REQ_FILE" || {
-        echo "Dependency conflict detected. Recreating venv..."
-        deactivate
-        rm -rf "$VENV_DIR"
-        python3 -m venv "$VENV_DIR"
-        source "$VENV_DIR/bin/activate"
-        pip install --upgrade pip
-        pip install -r "$REQ_FILE"
-    }
-    deactivate
-    exit 0
 fi
 
+echo "Activating venv and updating dependencies..."
 source "$VENV_DIR/bin/activate"
 pip install --upgrade pip
-pip install -r "$REQ_FILE"
+pip install -r "$REQ_FILE" || {
+    echo "Dependency conflict detected. Recreating venv..."
+    deactivate
+    rm -rf "$VENV_DIR"
+    python3 -m venv "$VENV_DIR"
+    source "$VENV_DIR/bin/activate"
+    pip install --upgrade pip
+    pip install -r "$REQ_FILE"
+}
+pip install pre-commit
+pre-commit install
 deactivate
 
-echo "venv is ready and dependencies are installed!"
+echo "venv is ready, dependencies and pre-commit are installed!"
